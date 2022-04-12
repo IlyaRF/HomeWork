24d315d4d39d070e52410eba952d5062c539c045
IlyaRF <ilyagaitanov@gmail.com>
2022-04-12 22:36:43 +0300

    third task

---
+++ src/main/java/ru/GB/Gaitanov/I/checkSumSign.java
@@ -0,0 +1,22 @@
+package ru.GB.Gaitanov.I;
+
+public class checkSumSign {
+
+    public static void main(String[] args) {
+        CheckSumSign();
+
+        int a = -10;
+        int b = 5;
+
+        if (a + b >= 0)
+        {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+
+    private static void CheckSumSign() {
+    }
+    }
