d401b58d1c2ee8b8d40aecee49cb54ff67a61c76
IlyaRF <ilyagaitanov@gmail.com>
2022-04-12 22:05:32 +0300

    second task

--- src/main/java/ru/GB/Gaitanov/I/HomeWorkApp.java
+++ src/main/java/ru/GB/Gaitanov/I/HomeWorkApp.java
@@ -3,6 +3,9 @@ package ru.GB.Gaitanov.I;
 public class HomeWorkApp {
     public static void main(String[] args) {
 
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
     }
 }
 
