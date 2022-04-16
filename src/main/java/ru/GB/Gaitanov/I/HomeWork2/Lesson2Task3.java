27556071a6f8c8a4dc20d9bd9eed2a70c3da896f
IlyaRF <ilyagaitanov@gmail.com>
2022-04-16 14:38:49 +0300

    third task

---
+++ src/main/java/ru/GB/Gaitanov/I/HomeWork2/Lesson2Task3.java
@@ -0,0 +1,18 @@
+package ru.GB.Gaitanov.I.HomeWork2;
+
+public class Lesson2Task3 {
+    public static void main(String[] args) {
+
+        System.out.println(within10and20(-10));
+        ;
+    }
+    public static boolean within10and20(int a) {
+
+        if (a < 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
