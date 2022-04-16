57c40a27ffea6b7ccd6f4750efb99573212990ab
IlyaRF <ilyagaitanov@gmail.com>
2022-04-16 14:11:55 +0300

    First Task

---
+++ src/main/java/ru/GB/Gaitanov/I/HomeWork2/Lesson2Task1.java
@@ -0,0 +1,24 @@
+package ru.GB.Gaitanov.I.HomeWork2;
+
+public class Lesson2Task1 {
+    public static void main(String[] args) {
+
+        System.out.println(within10and20(10, 11));
+        ;
+
+    }
+
+    public static boolean within10and20(int a, int b) {
+
+        if (a + b > 10 && a + b < 20) {
+            return true;
+        }
+        if (a + b < 10 || a + b > 20) {
+            return false;
+        }
+        return false;
+    }
+}
+
+
+
---
+++ src/main/java/ru/GB/Gaitanov/I/HomeWork2/Lesson2Task2.java
@@ -0,0 +1,15 @@
+package ru.GB.Gaitanov.I.HomeWork2;
+
+public class Lesson2Task2 {
+    public static void main(String[] args) {
+        int a = -5;
+
+        if (a >= 0) {
+            System.out.println("Число положительное");
+        } else  {
+            System.out.println("Число отрицательное");
+
+
+        }
+    }
+}
