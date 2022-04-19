fdb2a67906569e02bb6b12db7efdea8f44c556af
IlyaRF <ilyagaitanov@gmail.com>
2022-04-19 22:52:07 +0300

    second task

---
+++ src/main/java/ru/GB/Gaitanov/I/HomeWork3/Task2.java
@@ -0,0 +1,14 @@
+package ru.GB.Gaitanov.I.HomeWork3;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[100];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+            System.out.println(arr[i]);
+
+        }
+    }
+}
