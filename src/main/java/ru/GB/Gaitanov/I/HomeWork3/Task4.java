686e6c59870884c88b5c340bed226ce8d52ea13a
IlyaRF <ilyagaitanov@gmail.com>
2022-04-20 22:25:31 +0300

    fourth task

---
+++ src/main/java/ru/GB/Gaitanov/I/HomeWork3/Task4.java
@@ -0,0 +1,25 @@
+package ru.GB.Gaitanov.I.HomeWork3;
+
+public class Task4 {
+    public static void main(String[] args) {
+
+        int N = 4;
+        int arr[][] = new int[4][4];
+
+        for (int i = 0; i < N; i++) {
+            arr[i][i] = 1;
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++)
+
+                System.out.printf("%3d",arr[i][j]);
+                System.out.println();
+            }
+        }
+    }
+
+
+
+
+
+
