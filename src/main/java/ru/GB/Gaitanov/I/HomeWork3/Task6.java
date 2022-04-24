be381ad85a4f86e7dcc7836c89fd32f94eaef678
IlyaRF <ilyagaitanov@gmail.com>
2022-04-24 19:26:37 +0300

    sixfth task

--- src/main/java/ru/GB/Gaitanov/I/HomeWork3/Task6.java
+++ src/main/java/ru/GB/Gaitanov/I/HomeWork3/Task6.java
@@ -7,9 +7,9 @@ public class Task6 {
 
 
         int[] arr = new int[5];
-        arr[0] = 15;
-        arr[1] = 10;
-        arr[2] = 17;
+        arr[0] = 3;
+        arr[1] = 4;
+        arr[2] = -6;
         arr[3] = 8;
         arr[4] = 7;
 
