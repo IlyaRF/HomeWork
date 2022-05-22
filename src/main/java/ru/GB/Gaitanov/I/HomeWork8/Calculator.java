be21a68530bf5ad90e0e3d67a9556c73c75d3a11
IlyaRF <ilyagaitanov@gmail.com>
2022-05-23 01:43:17 +0300

    внес правки

--- src/main/java/ru/GB/Gaitanov/I/HomeWork8/Calculator.java
+++ src/main/java/ru/GB/Gaitanov/I/HomeWork8/Calculator.java
@@ -9,109 +9,22 @@ public class Calculator extends JFrame {
 
     public Calculator() {
 
-        setTitle("Test Window");
+        setTitle("Calculator");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setBounds(300, 300, 400, 400);
-        final JButton[] jButtons = new JButton[20];
-
-        jButtons[0] = new JButton("");
-        jButtons[1] = new JButton("CE");
-        jButtons[2] = new JButton("ROOT");
-        jButtons[3] = new JButton("%");
-        jButtons[4] = new JButton("7");
-        jButtons[5] = new JButton("8");
-        jButtons[6] = new JButton("9");
-        jButtons[7] = new JButton("/");
-        jButtons[8] = new JButton("4");
-        jButtons[9] = new JButton("5");
-        jButtons[10] = new JButton("6");
-        jButtons[11] = new JButton("X");
-        jButtons[12] = new JButton("1");
-        jButtons[13] = new JButton("2");
-        jButtons[14] = new JButton("3");
-        jButtons[15] = new JButton("-");
-        jButtons[16] = new JButton("0");
-        jButtons[17] = new JButton(".");
-        jButtons[18] = new JButton("=");
-        jButtons[19] = new JButton("+");
-
+        setLayout(new BorderLayout());
         JPanel numberPanel = new JPanel();
-        numberPanel.setLayout(numberPanel.getLayout());
-        final LayoutManager numberLayout = new GridLayout(5, 4, 10, 10);
-        setLayout(numberLayout);
-        setBounds(new Rectangle());
+        GridLayout numberLayout = new GridLayout(5, 4, 10, 10);
+        numberPanel.setLayout(numberLayout);
 
-        jButtons[1].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                jButtons[1].setText("on");
-            }
-        });
-        jButtons[2].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                jButtons[2].setText("root");
-            }
-        });
-        jButtons[3].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                jButtons[3].setText("%");
-            }
-        });
-        jButtons[4].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                jButtons[4].setText("/");
-            }
-        });
-        jButtons[5].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                jButtons[5].setText("X");
-            }
-        });
-        jButtons[6].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                jButtons[6].setText("-");
-            }
-        });
-        jButtons[7].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                jButtons[7].setText("+");
-            }
-        });
-        jButtons[8].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                jButtons[8].setText("=");
-            }
-        });
-        jButtons[9].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                jButtons[9].setText(".");
-            }
-        });
-
-        ActionListener btnListener = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                final JButton button = (JButton) e.getSource();
-                final String text = button.getText();
-                String btnNumber = text.substring(text.length() - 1);
-                jButtons[10].setText(btnNumber);
-
-            }
-        };
-        for (JButton jButton : jButtons) {
-            add(jButton);
-            jButton.addActionListener(btnListener);
-
+        for (int i = 0; i < 10; i++) {
+            final JButton jButtons = new JButton(String.valueOf(i));
+            numberPanel.add(jButtons);
             JLabel display = new JLabel("0");
             display.setHorizontalAlignment(SwingConstants.RIGHT);
+            display.setFont(new Font("Arial", Font.BOLD, 25));
+            add(display, BorderLayout.NORTH);
+            add(numberPanel, BorderLayout.CENTER);
         }
 
         setVisible(true);
