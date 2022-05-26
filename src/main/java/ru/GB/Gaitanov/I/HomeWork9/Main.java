6d46b9d5ae913cd0334c8ab215f1d22b709f13ea
IlyaRF <ilyagaitanov@gmail.com>
2022-05-27 00:02:09 +0300

    создал интерфейсы

--- src/main/java/ru/GB/Gaitanov/I/HomeWork9/Main.java
+++ src/main/java/ru/GB/Gaitanov/I/HomeWork9/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
 
-
         Cat[] cat = new Cat[3];
 
         cat[0] = new Cat("Murzik", 20, 5);
@@ -26,7 +25,69 @@ public class Main {
         robot[1] = new Robot("T-X", 50, 20);
 
 
-cat[0].run(5);
+
+
+        Run[] run = new Run[4];
+        run[0] = new Run() {
+            @Override
+            public void run(int r) {
+
+            }
+        };
+        run[1] = new Run() {
+            @Override
+            public void run(int r) {
+
+            }
+        };
+        run[2] = new Run() {
+            @Override
+            public void run(int r) {
+
+            }
+        };
+
+        run[3] = new Run() {
+            @Override
+            public void run(int r) {
+
+            }
+        };
+
+
+        Jump[] jump = new Jump[4];
+        jump[0] = new Jump() {
+
+            @Override
+            public void jump(int j) {
+
+            }
+        };
+        jump[1] = new Jump() {
+            @Override
+            public void jump(int j) {
+
+            }
+        };
+        jump[2] = new Jump() {
+            @Override
+            public void jump(int j) {
+
+            }
+        };
+
+        jump[3] = new Jump() {
+            @Override
+            public void jump(int j) {
+
+            }
+        };
+
+
+
+
+
+
     }
 
 
