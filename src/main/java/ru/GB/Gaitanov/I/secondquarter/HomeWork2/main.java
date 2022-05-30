91f21730f39eafde2ca77553544b58d9719da4b9
IlyaRF <ilyagaitanov@gmail.com>
2022-05-30 23:55:45 +0300

    объединил классы в общие массивы

---
+++ src/main/java/ru/GB/Gaitanov/I/secondquarter/HomeWork2/main.java
@@ -0,0 +1,28 @@
+package ru.GB.Gaitanov.I.secondquarter.HomeWork2;
+
+import java.lang.reflect.Array;
+
+public class main {
+    Array[][] arrays = new Array[4][4];
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void MyArraySizeException(String[] args) {
+        try {
+        } catch () {
+            System.out.println();
+
+        }
+
+        public static void MyArrayDataException (Array[i][i]) {
+            try {
+                Array[][]
+            } catch (MyArrayDataException e) {
+                System.out.println();
+            }
+        }
+    }
+}
+
