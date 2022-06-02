4a63487fc663ebd9e2145ea7c3966bcad92b0fb6
IlyaRF <ilyagaitanov@gmail.com>
2022-06-03 00:13:33 +0300

    Задание № 3

--- src/main/java/ru/GB/Gaitanov/I/secondquarter/HomeWork3/Phonebook.java
+++ src/main/java/ru/GB/Gaitanov/I/secondquarter/HomeWork3/Phonebook.java
@@ -1,8 +1,26 @@
 package ru.GB.Gaitanov.I.secondquarter.HomeWork3;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Phonebook {
+
+    static HashMap<String, Long> phonebook = new HashMap<>();
 
     public static void main(String[] args) {
 
+        add("Petrov", 89999990000L);
+
+
+    }
+
+    public static void add(String name, Long number) {
+        for (Map.Entry<String, Long> o : phonebook.entrySet()) {
+            System.out.println(o.getKey() + ": " + o.getValue());
+
+
+        }
     }
 }
+
