f263d5dab7c7554d9a1aa4aea46f037670684718
IlyaRF <ilyagaitanov@gmail.com>
2022-06-08 00:09:17 +0300

    ДЗ 5

--- src/main/java/ru/GB/Gaitanov/I/secondquarter/HomeWork5/MultiCore.java
+++ src/main/java/ru/GB/Gaitanov/I/secondquarter/HomeWork5/MultiCore.java
@@ -1,5 +1,7 @@
 package ru.GB.Gaitanov.I.secondquarter.HomeWork5;
 
+import java.util.Arrays;
+
 public class MultiCore {
     public static void main(String[] args) {
         threads();
@@ -18,6 +20,21 @@ public class MultiCore {
         }
         System.out.println("One thread time: " + (System.currentTimeMillis() -
                 startTime) + " ms.");
+        splitAndMergeExample();
 
+    }
+    public static void splitAndMergeExample() {
+        int[] initialArray = {1, 2, 3, 4, 5, 6};
+        System.out.println(Arrays.toString(initialArray));
+        int[] leftHalf = new int[3];
+        int[] rightHalf = new int[3];
+        System.arraycopy(initialArray, 0, leftHalf, 0, 3);
+        System.arraycopy(initialArray, 3, rightHalf, 0, 3);
+        System.out.println(Arrays.toString(leftHalf));
+        System.out.println(Arrays.toString(rightHalf));
+        int[] mergedArray = new int[6];
+        System.arraycopy(leftHalf, 0, mergedArray, 0, 3);
+        System.arraycopy(rightHalf, 0, mergedArray, 3, 3);
+        System.out.println(Arrays.toString(mergedArray));
     }
 }
