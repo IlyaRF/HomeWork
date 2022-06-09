e85023cff638bbd62d3d19aebe653bec93351260
IlyaRF <ilyagaitanov@gmail.com>
2022-06-09 21:16:30 +0300

    внес правки

--- src/main/java/ru/GB/Gaitanov/I/secondquarter/HomeWork2/Main.java
+++ src/main/java/ru/GB/Gaitanov/I/secondquarter/HomeWork2/Main.java
@@ -4,7 +4,6 @@ package ru.GB.Gaitanov.I.secondquarter.HomeWork2;
 public class Main {
 
 
-
     public static void main(String[] args) {
 
         String[][] number = new String[][]{{"1", "2", "3", "4", }, {"5", "6", "7", "8"}, {"9", "0", "9", "8"}, {"7", "6", "5", "4"}};
@@ -22,7 +21,7 @@ public class Main {
         }
     }
 
-    public static int check(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+    private static int check(String[][] arr) throws MyArraySizeException, MyArrayDataException {
         int count = 0;
         if (arr.length != 4) {
             throw new MyArraySizeException();
