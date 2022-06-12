76832e8e8766f59134dce255937c3f284063b192
IlyaRF <ilyagaitanov@gmail.com>
2022-06-13 00:22:54 +0300

    HM 6

--- src/main/java/ru/GB/Gaitanov/I/secondquarter/HomeWork6/EchoServer.java
+++ src/main/java/ru/GB/Gaitanov/I/secondquarter/HomeWork6/EchoServer.java
@@ -13,17 +13,20 @@ public class EchoServer {
             System.out.println("Сервер запущен, ожидаем подключения...");
             socket = serverSocket.accept();
             System.out.println("Клиент подключился");
-            DataInputStream in = new DataInputStream(socket.getInputStream());
+             DataInputStream in = new DataInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
             while (true) {
                 String str = in.readUTF();
                 if (str.equals("/end")) {
+                    out.writeUTF("/end");
                     break;
                 }
+                System.out.println("Сообщение от клиента");
                 out.writeUTF("Эхо: " + str);
             }
         } catch (IOException e) {
             e.printStackTrace();
+
         }
     }
 }
