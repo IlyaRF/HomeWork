44021fe570c5faa3f7e9e402bbf759063c50b05d
IlyaRF <ilyagaitanov@gmail.com>
2022-08-21 10:24:55 +0300

    Добавил метод в ArrayList

---
+++ src/main/java/ru/GB/Gaitanov/I/thirdquarter/HomeWork1/Apple.java
@@ -0,0 +1,4 @@
+package ru.GB.Gaitanov.I.thirdquarter.HomeWork1;
+
+public class Apple extends Fruit{
+}
--- src/main/java/ru/GB/Gaitanov/I/thirdquarter/HomeWork1/Change.java
+++ src/main/java/ru/GB/Gaitanov/I/thirdquarter/HomeWork1/Change.java
@@ -1,17 +1,30 @@
 package ru.GB.Gaitanov.I.thirdquarter.HomeWork1;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Arrays;
 
 public class Change {
 
     public static void main(String[] args) {
+        Integer[] ints = new Integer[]{1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(ints));
+        swap(ints, 2, 3);
+        System.out.println(Arrays.toString(ints));
     }
 
-    public static void swap(Map< Integer, Integer> array1) {
-        Map<String, Integer> map = new HashMap<>();
-        Map<Integer, String> swapped = map.entrySet().stream().collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
+    public static <T> void swap(T[] array, int firstIndex, int secondIndex) {
+        if (firstIndex < 0 || secondIndex < 0) {
+            throw new IllegalArgumentException();
         }
+        if (array == null) {
+            throw new IllegalArgumentException();
+        }
+        int length = array.length;
+        if (firstIndex > length || secondIndex > array.length) {
+            throw new IllegalArgumentException();
+        }
+        T tmp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = tmp;
+    }
     }
 
